b6a61d22c740f506e506d5981f6d6e26cbad43e7
vDawgg <veit.laule@gmail.com>
2024-05-13 20:15:28 +0200

    - Change formatting. - Add constants

--- utils/video/video_extraction_with_pytube.py
+++ utils/video/video_extraction_with_pytube.py
@@ -1,15 +1,16 @@
 from typing import List, Tuple, Union
 from pytube import YouTube
+from utils.video.constants import VIDEO_DIR
 
 
-def get_first_existing_key(keys_to_check :List[str], my_dict: dict)-> Union[str, None]:
+def get_first_existing_key(keys_to_check: List[str], my_dict: dict) -> Union[str, None]:
     for key in keys_to_check:
         if key in my_dict:
             return key  # Return the first key found
     return None  # Return None if no key is found
 
 
-def download_video_and_subtitles(url: str, output_path='./video/')-> Union[Tuple[str, str], Tuple[str, None]]:
+def download_video_and_subtitles(url: str, output_path=VIDEO_DIR) -> Union[Tuple[str, str], Tuple[str, None]]:
     """This function takes a YouTube video URL and an optional output path. It downloads the video and, if available, its subtitles in English or German.
 
     :param str url: The URL of the YouTube video.
@@ -28,22 +29,23 @@ def download_video_and_subtitles(url: str, output_path='./video/')-> Union[Tuple
 
             priority_languages = ['en', 'a.en', 'de', 'a.de']
             
-            caption_key= get_first_existing_key(priority_languages,yt.captions.keys())
+            caption_key = get_first_existing_key(priority_languages, yt.captions.keys())
 
             if not caption_key:
                 print("video has no captions in english or german")
-                return './video/'+video.default_filename, None
+                return output_path+video.default_filename, None
         
             subtitles = yt.captions[caption_key]
 
             if subtitles:
-                 return './video/'+video.default_filename, subtitles.generate_srt_captions()
+                return output_path+video.default_filename, subtitles.generate_srt_captions()
         else:
             print("No video found with mp4 format.")
     except Exception as e:
         print(f"Error downloading video: {e}")
 
+
 if __name__ == "__main__":
- path, subtitles =  download_video_and_subtitles("https://www.youtube.com/watch?v=HOlcUw465PA")
- print(subtitles)
+    path, subtitles = download_video_and_subtitles("https://www.youtube.com/watch?v=5C_HPTJg5ek")
+    print(subtitles)
 

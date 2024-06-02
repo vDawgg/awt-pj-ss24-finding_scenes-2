7a6d435ed9b1b75c38d0ffe7484195e6188812fc
liminm <liminmalek@gmail.com>
2024-06-02 10:44:03 +0200

    [WIP] add subtitles to prompt

--- utils/captioning/caption_keyframes.py
+++ utils/captioning/caption_keyframes.py
@@ -8,7 +8,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from utils.video.keyframe_captioning.model import Model
 
 
-
 def get_column_values(csv_file: str, column_name: str) -> List[str]:
     df = pd.read_csv(csv_file)
     return df[column_name].values.tolist()
@@ -21,7 +20,7 @@ def get_filepaths_from_csv(csv_file: str, filename_column: str, directory: str =
     return full_filepaths
 
 
-def caption_images(model: Model, prompt: str, csv_file: str = "extracted_keyframes.csv", filename_column: str = 'Filename', directory: str = "videos/keyframes") -> None:
+def caption_images(model: Model, base_prompt: str, tasks, csv_file: str = "extracted_keyframes.csv", filename_column: str = 'Filename', directory: str = "videos/keyframes") -> None:
     """
     Caption the images using the provided model and prompt.
 
@@ -37,19 +36,26 @@ def caption_images(model: Model, prompt: str, csv_file: str = "extracted_keyfram
     """
 
     full_filepaths = get_filepaths_from_csv(csv_file, filename_column, directory)
-
+    subtitle_list = get_column_values(csv_file, 'Subtitle')
     caption_list = []
 
-    for filepath in full_filepaths:
+    for filepath, subtitle in zip(full_filepaths, subtitle_list):
         image = Image.open(filepath)
         enc_image = model.encode_image(image)
-        prompt_response = model.run_inference(enc_image, prompt)
-        caption_list.append(prompt_response)
+        generated_captions = []
+        for task in tasks:
+            prompt = f"{base_prompt} \nSUBTITLES: {subtitle}\nTASK: {task}"
+            prompt_response = model.run_inference(enc_image, prompt)
+            generated_captions.append(prompt_response)
+        caption_list.append(generated_captions)
+
 
     csv_filepath = os.path.join(directory, csv_file)
     df = pd.read_csv(csv_filepath)
     df["Caption"] = caption_list
     df.to_csv(csv_filepath, index=True)
+
+    return csv_filepath
 
 
 if __name__ == "__main__":
@@ -62,10 +68,24 @@ if __name__ == "__main__":
     model_id = "vikhyatk/moondream2"
     revision = "2024-05-20"
     model = Model(model_id, revision)
-    prompt = "Describe the image with as much detail as possible. Generate as much information that can be turned into meta data as possible"
+
+    task_list = [
+        "Generate detailed information for this scene for TASK - instructions on what exactly to capture.",
+        "What is the language of the scene",
+        "What kind of scene is this",
+    ]
+
+    prompt = f"""
+    Given keyframe extracted from a scene and the corresponding SUBTITLES - the subtitles transcribed for this scene.
+    Generate detailed information for this scene for TASK - instructions on what exactly to capture.
+    Use both the image and the subtitles to infer the information.
+    If the TASK cannot be completed, then return "NONE".
+    """.strip()
 
     caption_images(
         model=model,
-        prompt=prompt,
+        base_prompt=prompt,
+        tasks=task_list,
         directory=directory
     )
+

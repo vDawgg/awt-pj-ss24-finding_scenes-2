ce0fe4993159f798979e4f2543aa31d7cb1035f0
liminm <liminmalek@gmail.com>
2024-06-03 10:16:12 +0200

    Fixed case when subtitles are not provided

--- utils/captioning/caption_keyframes.py
+++ utils/captioning/caption_keyframes.py
@@ -5,12 +5,15 @@ from PIL import Image
 import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from utils.video.keyframe_captioning.model import Model
+from utils.captioning.model import Model
 
 
 def get_column_values(csv_file: str, column_name: str) -> List[str]:
     df = pd.read_csv(csv_file)
-    return df[column_name].values.tolist()
+    if column_name in df.columns:
+        return df[column_name].values.tolist()
+    else:
+        return None
 
 
 def get_filepaths_from_csv(csv_file: str, filename_column: str, directory: str = "videos/keyframes") -> List[str]:
@@ -21,39 +24,29 @@ def get_filepaths_from_csv(csv_file: str, filename_column: str, directory: str =
 
 
 def caption_images(model: Model, base_prompt: str, tasks, csv_file: str = "extracted_keyframes.csv", filename_column: str = 'Filename', directory: str = "videos/keyframes") -> None:
-    """
-    Caption the images using the provided model and prompt.
-
-    Args:
-        model (object): The image captioning model.
-        prompt (str): The prompt to use for generating captions.
-        csv_file (str, optional): The name of the CSV file to store the captions. Defaults to "extracted_keyframes.csv".
-        filename_column (str, optional): The name of the column in the CSV file that contains the filenames. Defaults to 'Filename'.
-        directory (str, optional): The directory where the keyframe images are located. Defaults to "videos/keyframes".
-
-    Returns:
-        None
-    """
-
     full_filepaths = get_filepaths_from_csv(csv_file, filename_column, directory)
-    subtitle_list = get_column_values(csv_file, 'Subtitle')
+    subtitle_list = get_column_values(csv_file, 'SubtitleTEST')
     caption_list = []
 
-    for filepath, subtitle in zip(full_filepaths, subtitle_list):
+    for i, filepath in enumerate(full_filepaths):
         image = Image.open(filepath)
         enc_image = model.encode_image(image)
         task_responses = {}
         for task in tasks:
-            prompt = f"{base_prompt} \nSUBTITLES: {subtitle}\nTASK: {task}"
+            # Use subtitles if available, otherwise don't include them in the prompt
+            if subtitle_list is not None:
+                subtitle = subtitle_list[i]
+                prompt = f"{base_prompt} \nSUBTITLES: {subtitle}\nTASK: {task}"
+            else:
+                prompt = f"{base_prompt} \nTASK: {task}"
             prompt_response = model.run_inference(enc_image, prompt)
             task_responses[task] = prompt_response
         caption_list.append(task_responses)
 
-
     csv_filepath = os.path.join(directory, csv_file)
     df = pd.read_csv(csv_filepath)
     df["Caption"] = caption_list
-    df.to_csv(csv_filepath, index=True)
+    df.to_csv(csv_filepath, index=False)
 
     return csv_filepath
 
@@ -79,9 +72,13 @@ if __name__ == "__main__":
     prompt = f"""
     Given keyframe extracted from a scene and the corresponding SUBTITLES - the subtitles transcribed for this scene.
     Generate detailed information for this scene for TASK - instructions on what exactly to capture.
-    Use both the image and the subtitles to infer the information.
+    Use both the image and the SUBTITLES to infer the information.
+    If SUBTITLES is not provided, infer the information only from the keyframe image.
     If the TASK cannot be completed, then return "NONE".
     """.strip()
+
+
+
 
     csv_filepath = caption_images(
         model=model,

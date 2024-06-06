ed58b3a572a0ce0104f90f3423431c5b2da39b28
vDawgg <veit.laule@gmail.com>
2024-06-06 23:27:15 +0200

    Make VLM write to separate columns in the csv depending on the task.

--- utils/captioning/caption_keyframes.py
+++ utils/captioning/caption_keyframes.py
@@ -17,11 +17,13 @@ def get_column_values(csv_file: str, column_name: str) -> List[str]:
 def get_filepaths_from_csv(csv_file: str, filename_column: str, directory: str = "videos/keyframes") -> List[str]:
     csv_filepath = os.path.join(directory, csv_file)
     filenames = get_column_values(csv_filepath, filename_column)
-    full_filepaths = [f"{os.path.join(directory,''.join(filename.split('_')[:-1]),filename)}" for filename in filenames]
+    full_filepaths = [f"{os.path.join(directory, ''.join(filename.split('_')[:-1]), filename)}" for filename in
+                      filenames]
     return full_filepaths
 
 
-def caption_images(model: CaptionModel, base_prompt: str, tasks, csv_file: str = "extracted_keyframes.csv", filename_column: str = 'Filename', directory: str = "videos/keyframes") -> str:
+def caption_images(model: CaptionModel, base_prompt: str, tasks, csv_file: str = "extracted_keyframes.csv",
+                   filename_column: str = 'Filename', directory: str = "videos/keyframes") -> str:
     """
     Caption the images using the provided model and prompt.
 
@@ -39,26 +41,21 @@ def caption_images(model: CaptionModel, base_prompt: str, tasks, csv_file: str =
     csv_filepath = os.path.join(directory, csv_file)
     full_filepaths = get_filepaths_from_csv(csv_file, filename_column, directory)
     subtitle_list = get_column_values(csv_filepath, 'Subtitle')
-    caption_list = []
+    df = pd.read_csv(csv_filepath)
 
-    for i, filepath in enumerate(full_filepaths):
-        image = Image.open(filepath)
-        enc_image = model.encode_image(image)
-        task_responses = {}
-        for task in tasks:
-            # Use subtitles if available, otherwise don't include them in the prompt
+    for task, description in tasks.items():
+        task_outputs = []
+        for i, filepath in enumerate(full_filepaths):
+            image = Image.open(filepath)
+            enc_image = model.encode_image(image)
             if subtitle_list is not None:
                 subtitle = subtitle_list[i]
                 prompt = f"{base_prompt} \nSUBTITLES: {subtitle}\nTASK: {task}"
             else:
                 prompt = f"{base_prompt} \nTASK: {task}"
-            prompt_response = model.run_inference(enc_image, prompt)
-            task_responses[task] = prompt_response
-        caption_list.append(task_responses)
+            task_outputs.append(model.run_inference(enc_image, prompt))
+        df[task] = task_outputs
 
-    csv_filepath = os.path.join(directory, csv_file)
-    df = pd.read_csv(csv_filepath)
-    df["Caption"] = caption_list
     df.to_csv(csv_filepath, index=False)
 
     return csv_filepath
@@ -75,12 +72,12 @@ if __name__ == "__main__":
     revision = "2024-05-20"
     model = CaptionModel(model_id, revision=revision)
 
-    task_list = [
-        "Caption the scene. Describe it with as much information as possible."
-        "Generate detailed information for this scene for this scene.",
-        "What is the language used in the video this keyframe was captured from?",
-        "What kind of video is this, is it a tutorial, a lecture, and the likes.",
-    ]
+    tasks = {
+        "CAPTION": "Caption the scene. Describe it with as much information as possible.",
+        "INFORMATION": "Generate detailed information for this scene for this scene.",
+        "LANGUAGE": "What is the language used in the video this keyframe was captured from?",
+        "VIDEO_TYPE": "What kind of video is this, is it a tutorial, a lecture, and the likes.",
+    }
 
     prompt = f"""
     Given keyframe extracted from a scene and the corresponding SUBTITLES - the subtitles transcribed for this scene.
@@ -93,7 +90,7 @@ if __name__ == "__main__":
     csv_filepath = caption_images(
         model=model,
         base_prompt=prompt,
-        tasks=task_list,
+        tasks=tasks,
         directory=directory,
         csv_file=csv_filepath
     )

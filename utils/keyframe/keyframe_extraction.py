9477c9f4e7efd248c72afe3e25f1ef26aa9bdc4e
liminm <liminmalek@gmail.com>
2024-06-18 19:08:43 +0200

    Refactor keyframe extraction code for improved modularity and readability

--- utils/keyframe/keyframe_extraction.py
+++ utils/keyframe/keyframe_extraction.py
@@ -3,13 +3,17 @@ import csv
 import glob
 import time
 from typing import Optional
+from pathlib import Path
+import pandas as pd
+
 
 from Katna.writer import KeyFrameDiskWriter
 from katna_custom.custom_writer import TimeStampDiskWriter
 from katna_custom.custom_video_extraction import CustomVideo
+from utils.keyframe.utils import time_string_to_milliseconds, milliseconds_to_time_string
 
-from utils.keyframe.csv_creation import create_csv
-from utils.video.constants import VIDEO_DIR
+# from utils.video.constants import VIDEO_DIR
+from utils.constants import VIDEO_DIR
 
 
 def get_column_values_from_csv(
@@ -72,7 +76,7 @@ def extract_keyframe(
 
 def process_all_videos(
     video_files: list,
-    output_dir: str, 
+    output_dir: str,
     no_of_frames_to_return: int = 1
 ):
     """
@@ -97,8 +101,7 @@ def process_all_videos(
 
 
 def generate_keyframes_from_scenes(
-    scenes_csv_input_file: str,
-    keyframes_dir: str = None,
+    video_name: str,
     no_of_frames_to_return: int = 1
 ):
     """
@@ -113,12 +116,10 @@ def generate_keyframes_from_scenes(
         str: The path to the generated keyframe CSV file.
     """
 
+    scenes_csv_input_file = Path(VIDEO_DIR) / f"{video_name}_scenes" / "scene_list.csv"
     video_files = get_column_values_from_csv(scenes_csv_input_file, "file_name")
 
-    video_name = video_files[0].split("-Scene")[0]
-    
-    if keyframes_dir is None:
-        keyframes_dir = os.path.join(VIDEO_DIR, f"{video_name}_keyframes")
+    keyframes_dir = Path(VIDEO_DIR) / f"{video_name}_keyframes"
 
     os.makedirs(keyframes_dir, exist_ok=True)
 
@@ -129,22 +130,101 @@ def generate_keyframes_from_scenes(
         no_of_frames_to_return=no_of_frames_to_return
     )
 
-    # Create a new csv file with the keyframe information
-    keyframe_csv_path = create_csv(
-        keyframes_csv_input_dir=keyframes_dir,
-        scenes_csv_input_file=scenes_csv_input_file,
-        output_csv_filepath=os.path.join(keyframes_dir, "keyframes.csv")
-    )
-    
+    # Merge scene csv files into one
+    keyframe_csv_path = merge_csv(video_name=video_name)
+
     return keyframe_csv_path
+
+
+def generate_csv_file_paths(
+        keyframes_csv_input_dir: str = "videos/keyframes"
+) -> list:
+    """
+    Generate a list of all CSV files in the specific keyframe folders
+
+    Args:
+    keyframes_csv_input_dir (str): The directory containing the keyframe CSV files.
+
+    Returns:
+    list: A list of all CSV files in the specific keyframe folders.
+    """
+    # Get a list of all CSV files in the specific keyframe folders
+    csv_files = glob.glob(f"{keyframes_csv_input_dir}/*.csv")
+
+    return csv_files
+
+
+def merge_csv(
+        video_name: str,
+):
+    """
+    Creates a CSV file containing keyframe data by combining multiple CSV files and merging them with scene information.
+
+    Args:
+        keyframes_csv_input_dir (str): The directory path where the keyframe CSV files are located. Default is "videos/keyframes".
+        scenes_csv_input_file (str): The file path of the scene list CSV file. Default is "videos/video_scenes/scene_list.csv".
+        output_file (str): The file path of the output CSV file. Default is "videos/keyframes/extracted_keyframes.csv".
+
+    Returns:
+        str: The file path of the output CSV file.
+
+    """
+
+    video_name = "Rust in 100 Seconds"
+    csv_file_paths = Path(VIDEO_DIR) / f"{video_name}_keyframes"
+    merged_csv_filepath = Path(VIDEO_DIR) / f"{video_name}_keyframes" / "extracted_keyframes.csv"
+    scenes_csv_input_file = Path(VIDEO_DIR) / f"{video_name}_scenes" / "scene_list.csv"
+
+    csv_files = generate_csv_file_paths(csv_file_paths)
+
+    # Initialize an empty list to hold dataframes
+    dfs = []
+
+    # Loop over the list of CSV files
+    for csv_file in csv_files:
+        print(f"Reading {csv_file}")
+        # Read each CSV file into a DataFrame and append it to the list
+        df = pd.read_csv(csv_file)
+        dfs.append(df)
+        # Delete the CSV file
+        os.remove(csv_file)
+
+    # Concatenate all the dataframes in the list into a single dataframe
+    combined_df = pd.concat(dfs, ignore_index=True)
+
+    # Sort the combined dataframe based on 'Filename' column
+    combined_df = combined_df.sort_values('Filename')
+
+    # Read the scenes CSV file into a pandas DataFrame
+    scenes_df = pd.read_csv(scenes_csv_input_file)
+
+    scenes_df.rename(columns={'file_name': 'Source Filename'}, inplace=True)
+
+    # Merge the combined DataFrame with the scenes DataFrame on the 'Filename' column
+    merged_df = pd.merge(combined_df, scenes_df, on='Source Filename')
+
+    # Calculate the global timestamp by adding the local timestamp to the scene start time
+    timestring_scene = merged_df["Start Timecode"].astype(str).apply(time_string_to_milliseconds)
+    merged_df["Timestamp Global (ms)"] = merged_df["Timestamp Local (ms)"] + timestring_scene
+    merged_df["Timestamp Global (hh:mm:ss.SSS)"] = merged_df["Timestamp Global (ms)"].apply(milliseconds_to_time_string)
+
+    # Select only the desired columns from the merged DataFrame and save it to a new CSV file
+    final_df = merged_df[['Filename', 'Source Filename','Timestamp Local (ms)', 'Timestamp Local (hh:mm:ss.SSS)','Timestamp Global (ms)', 'Timestamp Global (hh:mm:ss.SSS)']]
+    final_df.to_csv(merged_csv_filepath, index=False, columns=['Filename', 'Source Filename', 'Timestamp Local (ms)', 'Timestamp Local (hh:mm:ss.SSS)','Timestamp Global (ms)', 'Timestamp Global (hh:mm:ss.SSS)'])
+
+    return merged_csv_filepath
 
 
 if __name__ == "__main__":
 
     start_time = time.time()
 
+    video_name = "Rust in 100 Seconds"
+
+    scenes_csv_input_file = Path(VIDEO_DIR) / f"{video_name}_scenes" / "scene_list.csv"
+
     keyframe_csv_path = generate_keyframes_from_scenes(
-        scenes_csv_input_file="videos/video_scenes/scene_list.csv",
+        video_name=video_name,
         no_of_frames_to_return=1
     )
 

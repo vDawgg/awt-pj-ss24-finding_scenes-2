0045c12dd266b799ab4690d144d921a5d8407a42
liminm <liminmalek@gmail.com>
2024-06-24 00:56:58 +0200

    Change from os.path to pathlib

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
 
-VIDEO_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'videos')
+# VIDEO_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'videos')
+VIDEO_DIR = Path(__file__).resolve().parent.parent / 'videos'
+
 HF_TOKEN = ""
